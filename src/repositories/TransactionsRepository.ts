7baf293c3d3149a7d2dc88db1715806639fbe8d9
unknown <hudson.oliveira.dias@gmail.com>
2020-09-15 16:09:13 -0300

    done

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,40 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number,
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
-
+  
   constructor() {
     this.transactions = [];
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.filter(transaction => transaction.type == 'income');
+    const incomeTotal = income.reduce((preVal, element) => preVal + element.value, 0);
+    const outcome = this.transactions.filter(transaction => transaction.type == 'outcome');
+    const outcomeTotal = outcome.reduce((preVal, element) => preVal + element.value, 0);
+    const balance = {
+      income: incomeTotal,
+      outcome: outcomeTotal,
+      total: incomeTotal - outcomeTotal
+    }
+    return balance
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction)
+    return transaction;
   }
 }
 
